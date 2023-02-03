45a6fe04b3dba3966e358b05ec9afa5182b8a04f
Ezra <ezrao@iastate.edu>
2023-02-03 09:03:09 -0600

    backend tutorial

--- Experiments/Ezra Experiments/helloworld/src/main/java/coms309/models/user.java
+++ Experiments/Ezra Experiments/helloworld/src/main/java/coms309/models/user.java
@@ -1,7 +1,35 @@
 package coms309.models;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "users")
 public class user {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id", nullable = false)
+    Long id;
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    private String userName;
+
+    private String password;
 
 
 }

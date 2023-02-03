36966e3140cbb5b6fd14d6d4fe57f5458f69b167
Ezra <ezrao@iastate.edu>
2023-02-03 14:53:10 -0600

    trying

--- Experiments/Ezra Experiments/helloworld/src/main/java/coms309/WelcomeController.java
+++ Experiments/Ezra Experiments/helloworld/src/main/java/coms309/WelcomeController.java
@@ -1,6 +1,7 @@
 package coms309;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PathVariable;
 
@@ -12,8 +13,11 @@ class WelcomeController {
         return "Hello and welcome to COMS 309";
     }
 
-    @GetMapping("/{name}")
-    public String welcome(@PathVariable String name) {
-        return "Hello and welcome to COMS 309: " + name;
+    @GetMapping("/{user}")
+    public String welcome(@PathVariable String user) {
+        return "Hello and welcome to COMS 309: " + user;
     }
+
+    @PostMapping("/users")
+    public String returnUsers(){return }
 }

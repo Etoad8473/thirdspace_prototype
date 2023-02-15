081e2c3874c6197bfda1265de836f929da84a34e
Ezra <ezrao@iastate.edu>
2023-02-15 09:48:10 -0600

    application properties

--- Backend/twin/src/main/java/twin/TwinApplication.java
+++ Backend/twin/src/main/java/twin/TwinApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TwinApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TwinApplication.class, args);
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(Application.class, args);
 	}
-
 }

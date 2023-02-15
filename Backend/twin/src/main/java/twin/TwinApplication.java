b0236038da88db92ece7d62745515c4db662c5b4
Ezra <ezrao@iastate.edu>
2023-02-15 09:34:51 -0600

    recreate base files

--- Backend/twin/src/main/java/twin/TwinApplication.java
+++ Backend/twin/src/main/java/twin/TwinApplication.java
@@ -1,13 +1,13 @@
-package com.example.demo;
+package twin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class TwinApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(TwinApplication.class, args);
 	}
 
 }
--- Backend/twin/src/test/java/twin/TwinApplicationTests.java
+++ Backend/twin/src/test/java/twin/TwinApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.demo;
+package twin;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class TwinApplicationTests {
 
 	@Test
 	void contextLoads() {

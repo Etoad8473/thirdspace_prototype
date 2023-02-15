356ac21a3558d64740c6dbee65657c20ac8afc7e
Ezra <ezrao@iastate.edu>
2023-02-15 09:55:22 -0600

    rebuilt project from pom and added outlines for controllers objets and repositories

--- Backend/twin/src/main/java/twin/TwinApplication.java
+++ Backend/twin/src/main/java/twin/TwinApplication.java
@@ -3,6 +3,11 @@ package twin;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Twin app
+ *
+ * @author Ezra n Charlie
+ */
 @SpringBootApplication
 public class TwinApplication {
 

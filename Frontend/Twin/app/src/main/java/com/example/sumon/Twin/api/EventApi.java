73927a07b0bf5e91b5abca9c809b104f964aca42
Kai Heng <kaiheng@kais-mbp.misc.iastate.edu>
2023-03-20 15:27:51 -0500

    update twin

---
+++ Frontend/Twin/app/src/main/java/com/example/sumon/Twin/api/EventApi.java
@@ -0,0 +1,22 @@
+package com.example.sumon.Twin.api;
+
+import com.example.sumon.Twin.model.Event;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.GET;
+import retrofit2.http.POST;
+import retrofit2.http.Path;
+
+public interface EventApi {
+    @GET("event")
+    Call<List<Event>> GetAllEvent();
+
+    @POST("users/{id}")
+    Call<Event> PostEventByPath(@Path("q") String question, @Path("a") String answer);
+
+    @POST("event")
+    Call<Event> PostEventByBody(@Body Event newEvent);
+}

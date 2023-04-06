5235786311e96eb51ef1e9777d61c0491aa3eaa4
Charlie Millar <crmillar@iastate.edu>
2023-04-06 13:36:28 -0500

    Created the message class

--- Backend/twin/src/main/java/twin/GroupChat/Message.java
+++ Backend/twin/src/main/java/twin/GroupChat/Message.java
@@ -1,4 +1,87 @@
 package twin.GroupChat;
 
+import twin.User.User;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "message")
 public class Message {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name="id")
+    private long id;
+
+    @Column(name = "data")
+    private String data;  //will store the message the user is sending
+
+    @Column(name = "date")
+    private String date; //will store the date sent
+
+    @Column(name = "time")
+    private String time; //time the message is sent
+
+    @OneToOne(cascade = {CascadeType.ALL})
+    @Column(name = "sender")
+    private User sender; //sender of the message
+
+    public Message(){
+
+    }
+
+    public Message(String data, String date, String time, User sender){
+        this.data = data;
+        this.date = date;
+        this.time = time;
+        this.sender = sender;
+    }
+
+    @Override
+    public String toString() {
+        return "Message{" +
+                "data=" + data +
+                ", date='" + date + '\'' +
+                ", time ='" + time + '\'' +
+                ", sender='" + sender + '\'' +
+                '}';
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getData(){
+        return data;
+    }
+
+    public void setData(String newData){
+        data = newData;
+    }
+
+    public String getDate(){
+        return date;
+    }
+
+    public void setDate(String newDate){
+        date = newDate;
+    }
+
+    public String getTime(){
+        return time;
+    }
+
+    public void setTime(String newTime){
+        time = newTime;
+    }
+
+    public User getSender(){
+        return sender;
+    }
+
+    public void setSender(User newSender){
+        sender = newSender;
+    }
+
+
 }

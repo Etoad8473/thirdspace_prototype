dc51889438fb63a08a8260e3b45f468a89582f0f
Charlie Millar <crmillar@iastate.edu>
2023-04-06 20:37:11 -0500

    Finished up GroupChat Controller

--- Backend/twin/src/main/java/twin/GroupChat/GroupChatController.java
+++ Backend/twin/src/main/java/twin/GroupChat/GroupChatController.java
@@ -1,9 +1,8 @@
 package twin.GroupChat;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import twin.Event.Event;
 import twin.User.User;
 
 import java.util.List;
@@ -22,5 +21,9 @@ public class GroupChatController {
     @GetMapping("/groupChat/{id}")
     public GroupChat getGroupChat(@PathVariable long id){ return groupChatRepo.findById(id);}
 
+    @PostMapping("/groupChat")
+    public GroupChat createGroupChat(@RequestBody GroupChat groupChat) { return groupChatRepo.save(groupChat); }
 
+    @DeleteMapping("/groupChat/{id}")
+    public @ResponseBody void removeGroupChat(@PathVariable Long id) { groupChatRepo.deleteById(id); }
 }

182d6fd9fa37370383f9678ffe406378d1e7aa30
Charlie Millar <crmillar@iastate.edu>
2023-04-26 17:32:38 -0500

    Added Swagger UI for backend

---
+++ Backend/twin/src/main/java/twin/SpringFoxConfig.java
@@ -0,0 +1,31 @@
+package twin;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SpringFoxConfig {
+    @Bean
+    public Docket api() {
+            return new Docket(DocumentationType.SWAGGER_2).host("http://coms-309-015.class.las.iastate.edu/docs/Server_side_does_by_Ezra_Odole_and_Charles_Millar")
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
+}
